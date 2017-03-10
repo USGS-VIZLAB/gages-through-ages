fba4dec47f295901f0922b3a448464b6f0703ca3
Jordan S Read <jread@usgs.gov>
2017-03-09 18:35:16 -0600

    bar chart start

---
+++ scripts/process/process_bar_chart.R
@@ -0,0 +1,18 @@
+process.bar_chart <- function(viz){
+  #data.in <- readDepends(viz)
+  
+  sites <- readr::read_csv('cache/allSitesYears_355.csv')
+  library(dplyr)
+  bars <- filter(sites, year >= viz[['min-year']], year <= viz[['max-year']]) %>% 
+    group_by(year) %>% tally %>% data.frame
+  
+  browser()
+  max.sites <- max(bars$n)
+  library(xml2)
+  g.bars <- read_xml("<g id='year-bars''/>")
+  for (bar in bars$year){
+    xml_add_child(g.bars, 'rect', x)
+  }
+  saveRDS(bars, file = viz[['location']])
+  
+}
