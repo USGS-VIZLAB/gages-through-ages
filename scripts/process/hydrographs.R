a15e4ba93f58ba0ea2e7cf58140a1543432bc7c2
Laura DeCicco <ldecicco@usgs.gov>
2017-03-10 22:32:33 -0600

    Clean up code

--- scripts/process/hydrographs.R
+++ scripts/process/hydrographs.R
@@ -11,58 +11,10 @@ process.hydrographData <- function(viz){
   
   data[['DayOfYear']] <- as.numeric(format(data[['Date']], "%j"))
   data[['Year']] <- as.numeric(format(data[['Date']], "%Y"))
-  data[['Flow.smooth']] <- runmean(data[['Flow']], smooth.days, endrule = "constant", align="left")
   
   saveRDS(data, file=viz[["location"]])
 }
 
 
-process.hydrographDataSVG <- function(viz=getContentInfo("processedNMdailySVG")){
-  data <- readData(viz[['depends']][['dailyData']])
-  pixelHeight <- viz[['pixelHeight']]
-  pixelWidth <- viz[['pixelWidth']]
-  
-  pixelCfs <- round(pixelHeight/max(data[['Flow.smooth']]), 5)
-  pixelDay <- getWidthOfDay(data$Date, pixelWidth)
-  
-  data <- dplyr::mutate(data, daysince=as.numeric(Date-min(Date)))
-  
-  data[['DateSVG']] <- data[['daysince']]*pixelDay
-  data[['FlowSVG']] <- data[['Flow.smooth']]*pixelCfs
-  
-  dataList <- list(data = data, pixelDay = pixelDay)
-  
-  saveRDS(dataList, file=viz[['location']])
-}
 
-process.hydrographRectangles <- function(viz){
-  dataList <- readData(viz[['depends']][['dailyDataSVG']])
-  data <- dataList[['data']]
-  
-  library(dplyr)
-  allYrs <- unique(data[['Year']])
-  
-  data <- group_by(data, Year)
-  data <- summarize(data, x=min(DateSVG), width=max(DateSVG) - min(DateSVG))
-  data <- mutate(data, 
-                 xleft=as.character(x), 
-                 xright=as.character(x+width))
-  
-  rect_specs <- by(data, 1:nrow(data), as.list)
-  names(rect_specs) <- paste0("y", allYrs)
-  
-  saveRDS(rect_specs, file=viz[["location"]])
-}
 
-#' handle date converion to SVG coords
-#' 
-#' @param date_vec a vector of all dates to use
-#' @param plot_width a single numeric value indicating the width
-#' of the plot in pixels.
-#' 
-#' @return SVG coords
-getWidthOfDay <- function(date_vec, plot_width){
-  total_num_days <- as.numeric(max(date_vec) - min(date_vec))
-  pixels_per_day <- plot_width/total_num_days
-  return(pixels_per_day)
-}
--- scripts/visualize/visualize-doy.R
+++ scripts/visualize/visualize-doy.R
@@ -8,6 +8,9 @@ visualize.doy <- function(viz = getContentInfo(viz.id = "doy-NM")){
   zoomer <- readData(viz[["depends"]][["zoomer"]])
   zoomer.xml <- read_xml(zoomer)
   
+  height <- viz[["height"]]
+  width <- viz[["width"]]
+  
   doy_svg = svglite::xmlSVG({
     par(omi=c(0,0,0,0), mai=c(0.5,0.75,0,0),las=1, xaxs = "i")
     plot(1, type="n", xlab="", frame.plot=FALSE,
@@ -15,7 +18,7 @@ visualize.doy <- function(viz = getContentInfo(viz.id = "doy-NM")){
          xlim=c(0, 366), ylim=c(0, 16000),xaxt="n")
     mtext(month.abb, 1, line = -1,
           at=c(15,46,74,105,135,166,196,227,258,288,319,349))
-  }, height=5, width=5.083333)
+  }, height=height, width=width)
   
   #################
   r <- xml_find_all(doy_svg, '//*[local-name()="rect"]')
@@ -28,9 +31,12 @@ visualize.doy <- function(viz = getContentInfo(viz.id = "doy-NM")){
   xml_attr(lines, "style") <- NULL
   # clean up junk that svglite adds:
   .junk <- lapply(r, xml_remove)
+
   #################
   xml_name(doy_svg, ns = character()) <- "g"
   xml_attr(doy_svg, "class") <- "axis-labels"
+  vb <- xml_attr(doy_svg, "viewBox")
+  xml_attr(doy_svg, "viewBox") <- NULL
   
   root <- xml_new_document() %>% xml_add_child("svg")
   xml_add_child(root, doy_svg)
@@ -39,48 +45,62 @@ visualize.doy <- function(viz = getContentInfo(viz.id = "doy-NM")){
   xml_attr(root, "xmlns") <- 'http://www.w3.org/2000/svg'
   xml_attr(root, "xmlns:xlink") <- 'http://www.w3.org/1999/xlink'
   xml_attr(root, "id") <- viz[["id"]]
-  xml_attr(root, "viewBox")
 
   xml_add_sibling(xml_children(root)[[1]], 'desc', .where='before', viz[["alttext"]])
   xml_add_sibling(xml_children(root)[[1]], 'title', .where='before', viz[["title"]])
   
-  grab_spark <- function(vals){
+  grab_spark <- function(vals, yMax, height, width){
     
     x = svglite::xmlSVG({
       par(omi=c(0,0,0,0), mai=c(0.5,0.75,0,0),
           las=1, mgp=c(2.5,0.25,0), xaxs = "i")
       plot(vals, type='l', axes=F, ann=F,
-           xlim=c(0, 366), ylim=c(0, 16000))
-    }, height=5, width=5.083333)
+           xlim=c(0, 366), ylim=c(0, yMax))
+    }, height=height, width=width)
     
   }
   
   g.doys <- xml_add_sibling(xml_children(root)[[length(xml_children(root))]], 'g', id='dayOfYear','class'='doy-polyline')
   
-  for(i in unique(daily$Year)){
+  yMax <- max(daily$Flow, na.rm = TRUE)
+  maxYear <- viz[["maxYear"]]
+  minYear <- viz[["minYear"]]
+  
+  years <- unique(daily$Year)
+  years <- years[years <= maxYear]
+  years <- years[years >= minYear]
+  
+  for(i in years){
     year_data <- filter(daily, Year == i) 
 
     if((i%%4 == 0) & ((i%%100 != 0) | (i%%400 == 0))){
       year_data$DayOfYear[year_data$DayOfYear>=60] <- year_data$DayOfYear[year_data$DayOfYear>=60]+1
     }
     
-    sub_data <- data.frame(approx(year_data$DayOfYear, year_data$Flow, n = 183))
+    sub_data <- data.frame(approx(year_data$DayOfYear, 
+                                  year_data$Flow, n = 366/viz[["full-desample"]]))
     
-    x <- grab_spark(sub_data)
-    polyline <- xml_children(x)[4]
+    x <- grab_spark(sub_data, yMax, height, width)
+    polyline <- xml_find_first(x, '//*[local-name()="polyline"]')
     xml_attr(polyline, "id") <- paste0("l",i)
     xml_attr(polyline, "class") <- "doy-lines-by-year"
     xml_attr(polyline, "clip-path") <- NULL
     xml_attr(polyline, "style") <- NULL
-    xml_add_child(g.doys, polyline[[1]])
+    xml_attr(polyline, "viewBox") <- NULL
+    xml_add_child(g.doys, polyline)
   }
   
   g.zoomer <- xml_add_sibling(xml_children(root)[[length(xml_children(root))]], 'g', id='total_hydro','class'='total_hydro')
-  xml_attr(g.zoomer, "transform") <- "translate(0,350)"
   
+  vb <- as.numeric(strsplit(vb, ' ')[[1]])
+  xml_attr(g.zoomer, 'transform') <- sprintf("translate(0,%s)", vb[4])
+
   xml_add_child(g.zoomer, zoomer.xml)
   
-  xml_attr(root, "viewBox") <- paste(c(0,0,366,450), collapse=' ')
+  h <- xml_find_all(g.zoomer, '//*[local-name()="rect"]') %>% xml_attr('height') %>% as.numeric() %>% max
+  vb[4] <- vb[4] + h*1.5
+  
+  xml_attr(root, "viewBox") <- paste(vb, collapse=' ')
   
   write_xml(root, viz[["location"]])
   
